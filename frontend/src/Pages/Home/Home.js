dd4e82dd3e877f41cdd1ffbddc9506d25d7039d9
ramos-r <bckymoon@hotmail.com>
2020-09-17 22:21:24 -0300

    Estilo dos cards e footer terminado, imagens e form adicionados

--- frontend/src/Pages/Home/Home.js
+++ frontend/src/Pages/Home/Home.js
@@ -3,6 +3,19 @@ import './Home.css';
 import Logo from '../../Assets/logo.png'
 import GitLogo from '../../Assets/github-logo.svg'
 import card1 from '../../Assets/img-cards/card-vidal.png'
+import card2 from '../../Assets/img-cards/card-felipe.png'
+import card3 from '../../Assets/img-cards/card-rebecca.png'
+import card4 from '../../Assets/img-cards/card-victor.png'
+import img1 from '../../Assets/img-cards/Awwwards.png'
+import img2 from '../../Assets/img-cards/Behance.png'
+import img3 from '../../Assets/img-cards/Coder.png'
+import img4 from '../../Assets/img-cards/Habitica.png'
+import img5 from '../../Assets/img-cards/LouisVuitton.png'
+import img6 from '../../Assets/img-cards/McDonalds.png'
+import img7 from '../../Assets/img-cards/Rocketseat.png'
+import img8 from '../../Assets/img-cards/Rubi.png'
+import img9 from '../../Assets/img-cards/StudioBrunner.png'
+import img10 from '../../Assets/img-cards/Vogue.png'
 
 function Home() {
     return(
@@ -26,34 +39,36 @@ function Home() {
             {/* Nesta section falta adicionar os botões com link para a página e os prints de cada uma das mesmas. */}
             <section className="cards">
                 <div className="card">
-                    <img src="" alt=""/>
+                    <img src={card2} alt="card-felipe"/>
 
                     <div className="title">
-                        <h3>Felipe // Page</h3>
+                        <h3>Felipe Duarte</h3>
                     </div>
                 </div>
 
                 <div className="card">
-                    {/* <img src={card1} alt=""/> */}
+                    <img src={card1} alt="card-vidal"/>
 
                     <div className="title">
-                        <h3>Matheus // Page</h3>
+                        <h3>Matheus Vidal</h3>
                     </div>
                 </div>
 
                 <div className="card">
-                    <img src="" alt=""/>
+                    <a href="https://ramos-r.github.io/workshop-react-pokedex/">
+                        <img src={card3} alt="card-rebecca"/>
+                    </a>
 
                     <div className="title">
-                        <h3>Rebecca // Page</h3>
+                        <h3>Rebecca Ramos</h3>
                     </div>
                 </div>
 
                 <div className="card">
-                    <img src="" alt=""/>
+                    <img src={card4} alt="card-victor"/>
 
                     <div className="title">
-                        <h3>Victor // Page</h3>
+                        <h3>Victor Bhering</h3>
                     </div>
                 </div>
             </section>
@@ -67,39 +82,87 @@ function Home() {
                 <div className="cards2">
 
                     <div className="card2">
+                        <a href="https://www.awwwards.com/">
+                            <img src={img1} alt="Awwwards.png" className=""/>
+                        </a>
                     </div>
 
                     <div className="card2">
+                        <a href="https://www.behance.net/galleries">
+                            <img src={img2} alt="Behance.png" className=""/>
+                        </a>
                     </div>
 
                     <div className="card2">
+                        <a href="https://coder.com/">
+                            <img src={img3} alt="Coder.png" className=""/>
+                        </a>
                     </div>
 
                     <div className="card2">
+                        <a href="https://habitica.com/static/home">
+                            <img src={img4} alt="Habitica.png" className=""/>
+                        </a>
                     </div>
 
                     <div className="card2">
+                        <a href="https://br.louisvuitton.com/por-br/homepage">
+                            <img src={img5} alt="LouisVuitton.png" className=""/>
+                        </a>
                     </div>
 
                     <div className="card2">
+                        <a href="https://www.mcdonalds.com.br/">
+                            <img src={img6} alt="McDonalds.png" className=""/>
+                        </a>
                     </div>
 
                     <div className="card2">
+                        <a href="https://rocketseat.com.br/">
+                            <img src={img7} alt="Rocketseat.png" className=""/>
+                        </a>
                     </div>
 
                     <div className="card2">
+                        <a href="https://www.lojasrubi.com.br/">
+                            <img src={img8} alt="Rubi.png" className=""/>
+                        </a>
                     </div>
 
                     <div className="card2">
+                        <a href="https://www.studiobrunner.com/">
+                            <img src={img9} alt="StudioBrunner.png" className=""/>
+                        </a>
                     </div>
 
                     <div className="card2">
+                        <a href="https://www.vogue.pt/">
+                            <img src={img10} alt="Vogue.png" className=""/>
+                        </a>
                     </div>
 
                 </div>
             </section>
             
+            <form onSubmit={handleFormSubmit}>
+        <label htmlFor="email">E-mail</label>
+        <input type="text" id="email" name="email" placeholder="E-mail de destino.." onChange={handleInputChange} />
 
+
+        <label htmlFor="nome">Nome</label>
+        <input type="text" id="nome" name="nome" placeholder="Nome da pessoa.." onChange={handleInputChange} />
+
+
+        <label htmlFor="mensagem">Mensagem</label>
+        <textarea id="mensagem" name="mensagem" placeholder="Escreva algo.." className="textArea" onChange={handleInputChange}></textarea>
+
+
+        <label htmlFor="anexo">Anexo</label>
+        <input type="file" id="anexo" name="anexo" onChange={handleInputChange} />
+
+
+        <input type="submit" value="Enviar" />
+      </form>
 
             <section className="footer">
                 <div className="footer-title">
